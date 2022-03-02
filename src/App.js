41f9baa27551c8b96de8422891087a7d94fa039e
Carolina9503 <69082421+Carolina9503@users.noreply.github.com>
2022-03-01 20:20:15 -0500

    timer running

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [minutesInput, setMinutesInput] = useState("");
+  const [secondsInput, setSecondsInput] = useState("");
+  const [isStarted, setIsStarted] = useState(false);
+  const [time, setTime] = useState(0);
+
+  const handleReset = () => {
+    setTime(0);
+    setIsStarted(false);
+    setMinutesInput("");
+    setSecondsInput("");
+  };
+  const handleStart = () => {
+    setTime(Number(minutesInput) * 60 + Number(secondsInput));
+    setIsStarted(true);
+  };
+  useEffect(() => {
+    const timer = setInterval(() => {
+      if (isStarted && time > 0) {
+        setTime(time - 1);
+      }
+    }, 1000);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, [isStarted, time]);
+
+  const formatTime = () => {
+    const minutes = Math.floor(time / 60);
+    const seconds = time - minutes * 60;
+    return {
+      minutes,
+      seconds,
+    };
+  };
+  const formattedTimer = formatTime();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <label>
+        <input
+          type="number"
+          value={minutesInput}
+          onChange={(event) => setMinutesInput(event.target.value)}
+        />
+        Minutes
+      </label>
+      <label>
+        <input
+          type="number"
+          value={secondsInput}
+          onChange={(event) => setSecondsInput(event.target.value)}
+        />
+        Seconds
+      </label>
+      <button onClick={handleStart}>START</button>
+      <button onClick={() => setIsStarted(!isStarted)}>PAUSE / RESUME</button>
+      <button onClick={handleReset}>RESET</button>
+
+      <h1>
+        {`${
+          formattedTimer.minutes < 10
+            ? "0" + formattedTimer.minutes
+            : formattedTimer.minutes
+        }`}
+        :
+        {`${
+          formattedTimer.seconds < 10
+            ? "0" + formattedTimer.seconds
+            : formattedTimer.seconds
+        }`}
+      </h1>
     </div>
   );
 }
